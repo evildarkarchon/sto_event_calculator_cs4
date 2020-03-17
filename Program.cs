25afb15dbfdf67ba6738f6663bc384c42f7bdaf0
Andrew Nelson <evildarkarchon@gmail.com>
2020-03-17 00:07:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sto_event_calculator_cs4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
